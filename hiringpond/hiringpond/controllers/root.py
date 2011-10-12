dd0e537378ab7539d9f3857ebc1ee7aaf9bfd156
Michael J. Pedersen <m.pedersen@icelus.org>
2011-10-12 08:37:28 -0400

    Committing last night's work so I can work on the laptop.

--- hiringpond/hiringpond/controllers/root.py
+++ hiringpond/hiringpond/controllers/root.py
@@ -4,13 +4,11 @@
 from tg import expose, flash, require, url, request, redirect
 from tg.i18n import ugettext as _, lazy_ugettext as l_
 from tgext.admin.tgadminconfig import TGAdminConfig
-from tgext.admin.controller import AdminController
 from repoze.what import predicates
 
 from hiringpond.lib.base import BaseController
 from hiringpond.model import DBSession, metadata
 from hiringpond import model
-from hiringpond.controllers.secure import SecureController
 
 from hiringpond.controllers.error import ErrorController
 
@@ -31,48 +29,12 @@ class RootController(BaseController):
     must be wrapped around with :class:`tg.controllers.WSGIAppController`.
 
     """
-    secc = SecureController()
-
-    admin = AdminController(model, DBSession, config_type=TGAdminConfig)
-
     error = ErrorController()
 
     @expose('hiringpond.templates.index')
     def index(self):
         """Handle the front-page."""
         return dict(page='index')
-
-    @expose('hiringpond.templates.about')
-    def about(self):
-        """Handle the 'about' page."""
-        return dict(page='about')
-
-    @expose('hiringpond.templates.environ')
-    def environ(self):
-        """This method showcases TG's access to the wsgi environment."""
-        return dict(environment=request.environ)
-
-    @expose('hiringpond.templates.data')
-    @expose('json')
-    def data(self, **kw):
-        """This method showcases how you can use the same controller for a data page and a display page"""
-        return dict(params=kw)
-
-    @expose('hiringpond.templates.authentication')
-    def auth(self):
-        """Display some information about auth* on this application."""
-        return dict(page='auth')
-    @expose('hiringpond.templates.index')
-    @require(predicates.has_permission('manage', msg=l_('Only for managers')))
-    def manage_permission_only(self, **kw):
-        """Illustrate how a page for managers only works."""
-        return dict(page='managers stuff')
-
-    @expose('hiringpond.templates.index')
-    @require(predicates.is_user('editor', msg=l_('Only for the editor')))
-    def editor_user_only(self, **kw):
-        """Illustrate how a page exclusive for the editor works."""
-        return dict(page='editor stuff')
 
     @expose('hiringpond.templates.login')
     def login(self, came_from=url('/')):
